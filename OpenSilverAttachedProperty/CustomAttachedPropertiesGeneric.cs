34a0f6764a690573579859366be81f28b97a8eeb
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-04-13 19:19:45 +1000

    Add project files.

---
+++ OpenSilverAttachedProperty/CustomAttachedPropertiesGeneric.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace OpenSilverAttachedProperty
+{
+    internal class CustomAttachedPropertiesGeneric<TA> : DependencyObject
+    {
+        // Define the MyTextProperty attached property
+        public static readonly DependencyProperty MyStorageProperty =
+            DependencyProperty.RegisterAttached("MyStorage", typeof(string), typeof(CustomAttachedPropertiesGeneric<TA>), new PropertyMetadata(null));
+
+        // Get method for the MyTextProperty attached property
+        public static TA GetMyStorage(DependencyObject obj)
+        {
+            return (TA)obj.GetValue(MyStorageProperty);
+        }
+
+        // Set method for the MyTextProperty attached property
+        public static void SetMyStorage(DependencyObject obj, TA value)
+        {
+            obj.SetValue(MyStorageProperty, value);
+        }
+    }
+}
---
+++ OpenSilverAttachedProperty/CustomAttachedPropertiesString.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSilverAttachedProperty
+{
+    internal class CustomAttachedPropertiesString : CustomAttachedPropertiesGeneric<string>
+    {
+    }
+}
